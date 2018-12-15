c6053d9dd21bcce3d102325894b6d06238719c2e
DrVrej <DrVrej@users.noreply.github.com>
2018-12-14 19:15:46 -0500

    Rebel Sound Change
    Rebel Sound Change

--- lua/entities/npc_vj_hl_aliengrunt/init.lua
+++ lua/entities/npc_vj_hl_aliengrunt/init.lua
@@ -5,10 +5,9 @@ include('shared.lua')
 	No parts of this code or any of its contents may be reproduced, copied, modified or adapted,
 	without the prior written consent of the author, unless otherwise indicated for stand-alone materials.
 -----------------------------------------------*/
-ENT.Model = {"models/cpthazama/hydra.mdl"} -- The game will pick a random model from the table when the SNPC is spawned | Add as many as you want
+ENT.Model = {"models/cpthazama/starwars/droideka.mdl"} -- The game will pick a random model from the table when the SNPC is spawned | Add as many as you want
 ENT.StartHealth = 500
 ENT.HullType = HULL_HUMAN
-ENT.MovementType = VJ_MOVETYPE_STATIONARY -- How does the SNPC move?
 ---------------------------------------------------------------------------------------------------------------------------------------------
 ENT.BloodColor = "Red" -- The blood type, this will determine what it should use (decal, particle, etc.)
 
