04a575eb898087b391819fa872d1d01d791e24f6
DrVrej <DrVrej@users.noreply.github.com>
2020-01-03 01:01:49 -0500

    Fully Added Protozoan!

--- lua/entities/npc_vj_hlr1_protozoan/init.lua
+++ lua/entities/npc_vj_hlr1_protozoan/init.lua
@@ -8,8 +8,7 @@ include('shared.lua')
 ENT.Model = {"models/vj_hlr/hl1/protozoa.mdl"} -- The game will pick a random model from the table when the SNPC is spawned | Add as many as you want
 ENT.StartHealth = 5
 ENT.HullType = HULL_TINY
-ENT.TurningSpeed = 12 -- How fast it can turn
-ENT.TurningUseAllAxis = true -- If set to true, angles will not be restricted to y-axis, it will change all axes (plural axis)
+ENT.TurningSpeed = 1 -- How fast it can turn
 ENT.MovementType = VJ_MOVETYPE_AERIAL -- How does the SNPC move?
 ENT.Aerial_FlyingSpeed_Calm = 100 -- The speed it should fly with, when it's wandering, moving slowly, etc. | Basically walking campared to ground SNPCs
 ENT.Aerial_FlyingSpeed_Alerted = 100 -- The speed it should fly with, when it's chasing an enemy, moving away quickly, etc. | Basically running campared to ground SNPCs
@@ -21,10 +20,8 @@ ENT.Behavior = VJ_BEHAVIOR_PASSIVE_NATURE -- The behavior of the SNPC
 ENT.HasMeleeAttack = false -- Should the SNPC have a melee attack?
 	-- ====== Sound File Paths ====== --
 -- Leave blank if you don't want any sounds to play
-ENT.SoundTbl_Idle = {}
-ENT.SoundTbl_Pain = {}
-ENT.SoundTbl_Death = {}
-
+ENT.SoundTbl_Idle = {"vj_hlr/hl1_npc/protozoan/chirp03.wav","vj_hlr/hl1_npc/protozoan/chirp04.wav","vj_hlr/hl1_npc/protozoan/chirp05.wav","vj_hlr/hl1_npc/protozoan/chirp06.wav","vj_hlr/hl1_npc/protozoan/chirp07.wav"}
+ENT.SoundTbl_Death = {"vj_hlr/hl1_npc/protozoan/chirp03.wav","vj_hlr/hl1_npc/protozoan/chirp04.wav","vj_hlr/hl1_npc/protozoan/chirp05.wav","vj_hlr/hl1_npc/protozoan/chirp06.wav","vj_hlr/hl1_npc/protozoan/chirp07.wav"}
 ---------------------------------------------------------------------------------------------------------------------------------------------
 function ENT:CustomOnInitialize()
 	self:SetCollisionBounds(Vector(28, 28, 65), Vector(-28, -28, 0))
