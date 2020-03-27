dd8a33f452d75b23f3118d359304dbbbcc94ce2f
DrVrej <DrVrej@users.noreply.github.com>
2020-03-27 16:31:21 -0400

    Revamped Shock Trooper Weapon

--- lua/weapons/weapon_vj_hl_strooperwep/shared.lua
+++ lua/weapons/weapon_vj_hl_strooperwep/shared.lua
@@ -6,13 +6,7 @@ SWEP.Author 					= "DrVrej"
 SWEP.Contact					= "http://steamcommunity.com/groups/vrejgaming"
 SWEP.Purpose					= "This weapon is made for Players and NPCs"
 SWEP.Instructions				= "Controls are like a regular weapon."
--- SWEP.Category					= "VJ Base"
-	-- Client Settings ---------------------------------------------------------------------------------------------------------------------------------------------
-if (CLIENT) then
-SWEP.Slot						= 2 -- Which weapon slot you want your SWEP to be in? (1 2 3 4 5 6) 
-SWEP.SlotPos					= 4 -- Which part of that slot do you want the SWEP to be in? (1 2 3 4 5 6)
-SWEP.UseHands					= true
-end
+SWEP.Category					= "VJ Base"
 	-- NPC Settings ---------------------------------------------------------------------------------------------------------------------------------------------
 SWEP.NPC_NextPrimaryFire 		= false -- Next time it can use primary fire
 SWEP.NPC_ReloadSound			= {"vj_hlr/hl1_weapon/shockroach/shock_recharge.wav"} -- Sounds it plays when the base detects the SNPC playing a reload animation
@@ -31,10 +25,7 @@ SWEP.WorldModel_CustomPositionOrigin = Vector(20,3,-2.5)
 SWEP.WorldModel_CustomPositionBone = "Bone58" -- The bone it will use as the main point
 	-- Primary Fire ---------------------------------------------------------------------------------------------------------------------------------------------
 SWEP.Primary.Damage				= 5 -- Damage
--- SWEP.Primary.ClipSize			= 10 -- Max amount of bullets per clip
 SWEP.Primary.ClipSize			= 30 -- Max amount of bullets per clip
-SWEP.Primary.Delay				= 0.2 -- Time until it can shoot again
-SWEP.Primary.Automatic			= true -- Is it automatic?
 SWEP.Primary.Ammo				= "SMG1" -- Ammo type
 SWEP.Primary.Sound				= {"vj_hlr/hl1_weapon/shockroach/shock_fire.wav"}
 SWEP.Primary.DisableBulletCode	= true
@@ -63,21 +54,21 @@ end
 ---------------------------------------------------------------------------------------------------------------------------------------------
 function SWEP:CustomOnPrimaryAttack_BeforeShoot()
 	if (CLIENT) then return end
+	
 	local plasma = ents.Create("obj_vj_hlrof_plasma")
-	plasma:SetPos(self:GetOwner():GetBonePosition(self:GetOwner():LookupBone(self.WorldModel_CustomPositionBone)))
+	plasma:SetPos(self:GetNWVector("VJ_CurBulletPos"))
 	plasma:SetAngles(self:GetOwner():GetAngles())
 	plasma:SetOwner(self:GetOwner())
-	plasma:Activate()
 	plasma:Spawn()
-
-	//phy:SetVelocity(self:GetOwner():CalculateProjectile("Line", self:GetOwner():GetPos(), self:GetOwner():GetEnemy():GetPos(), 10000))
-	local phy = plasma:GetPhysicsObject()
-	if phy:IsValid() then
-		local pos = self:GetOwner():GetPos() +self:GetOwner():OBBCenter() +self:GetOwner():GetForward() *700
+	plasma:Activate()
+	
+	local phys = plasma:GetPhysicsObject()
+	if IsValid(phys) then
+		local pos = self:GetOwner():GetPos() + self:GetOwner():OBBCenter() + self:GetOwner():GetForward() * 700
 		if IsValid(self:GetOwner():GetEnemy()) then
 			pos = self:GetOwner():GetEnemy():GetPos()
 		end
-		phy:ApplyForceCenter(((pos -self:GetOwner():GetRight() *20) - self:GetOwner():GetPos()) *150)
+		phys:SetVelocity(self:GetOwner():CalculateProjectile("Line", self:GetNWVector("VJ_CurBulletPos"), pos, 10000))
 	end
 end
 ---------------------------------------------------------------------------------------------------------------------------------------------
@@ -91,31 +82,22 @@ function SWEP:CustomOnNPC_ServerThink()
 	
 	if CurTime() > self.HLR_NextIdleSoundT then
 		if IsValid(self:GetOwner():GetEnemy()) then
-			self:EmitSound("vj_hlr/hl1_npc/shockroach/shock_angry.wav",70,100)
+			self:EmitSound("vj_hlr/hl1_npc/shockroach/shock_angry.wav", 70)
 		else
-			self:EmitSound("vj_hlr/hl1_npc/shockroach/shock_idle" .. math.random(1,3) .. ".wav",65,100)
+			self:EmitSound("vj_hlr/hl1_npc/shockroach/shock_idle" .. math.random(1,3) .. ".wav", 65)
 		end
 		self.HLR_NextIdleSoundT = CurTime() +math.Rand(5,12)
 	end
 end
 ---------------------------------------------------------------------------------------------------------------------------------------------
-function SWEP:CustomOnRemove()
-	if IsValid(self:GetOwner()) then self:GetOwner():SetBodygroup(1,1) end
-end
----------------------------------------------------------------------------------------------------------------------------------------------
 function SWEP:CustomOnPrimaryAttackEffects()
 	if self.PrimaryEffects_MuzzleFlash == true && GetConVarNumber("vj_wep_nomuszzleflash") == 0 then
-		-- ParticleEffectAttach(VJ_PICK(self.PrimaryEffects_MuzzleParticles),PATTACH_POINT_FOLLOW,self:GetOwner(),self:GetOwner():LookupAttachment(self:GetNWInt("VJ_HGrunt_BulletAttachmet")))
+		ParticleEffect("vj_hl_shockroach", self:GetNWVector("VJ_CurBulletPos"), self:GetNWVector("VJ_CurBulletPos"):Angle(), self:GetOwner())
+		timer.Simple(0.05, function() if IsValid(self) && IsValid(self:GetOwner()) then self:GetOwner():StopParticles() end end)
 	end
 	return false
 end
 ---------------------------------------------------------------------------------------------------------------------------------------------
-function SWEP:CustomOnDrawWorldModel() -- This is client only!
-	if IsValid(self:GetOwner()) then
-		self.WorldModel_Invisible = true
-		return false
-	else
-		self.WorldModel_Invisible = false
-		return true -- return false to not draw the world model
-	end
+function SWEP:CustomOnRemove()
+	if IsValid(self:GetOwner()) then self:GetOwner():SetBodygroup(1,1) end
 end
