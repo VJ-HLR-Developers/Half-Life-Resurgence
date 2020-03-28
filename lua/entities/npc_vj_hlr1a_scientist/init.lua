7408ea06f96bf9c93be5ff09bda3d513b255b568
Mawskeeto1223 <tomaxx11@gmail.com>
2020-03-28 23:40:05 +0200

    Fixed Nihilanth's head attachment + removed clientside ragdoll from Alpha Scientist

--- lua/entities/npc_vj_hlr1a_scientist/init.lua
+++ lua/entities/npc_vj_hlr1a_scientist/init.lua
@@ -7,7 +7,7 @@ include('shared.lua')
 -----------------------------------------------*/
 ENT.Model = {"models/vj_hlr/hla/scientist.mdl"} -- The game will pick a random model from the table when the SNPC is spawned | Add as many as you want
 ENT.IsMedicSNPC = false -- Is this SNPC a medic? Does it heal other friendly friendly SNPCs, and players(If friendly)
-
+ENT.DeathAnimationTime = 5
 	-- ====== Controller Variables ====== --
 ENT.Controller_FirstPersonBone = "unnamed021"
 ENT.Controller_FirstPersonOffset = Vector(2,0,5)
@@ -19,13 +19,7 @@ function ENT:SCI_CustomOnInitialize()
 	self.SoundTbl_Death = {"vj_hlr/hla_npc/barney/ba_die1.wav","vj_hlr/hla_npc/barney/ba_die2.wav","vj_hlr/hla_npc/barney/ba_die3.wav"}
 	self.SoundTbl_IdleDialogueAnswer = {"vj_hlr/hla_npc/barney/ba_pain1.wav"}
 	self:SetBodygroup(0,math.random(0,4))
-end
----------------------------------------------------------------------------------------------------------------------------------------------
-// apparently they said no. my pleas were ignored.
-function ENT:CustomDeathAnimationCode(dmginfo,hitgroup)
-	self.DeathAnimationTime = 5
-	self.HasDeathRagdoll = false
-	self:Fire("BecomeRagdoll","",1.6)
+	
 end
 /*-----------------------------------------------
 	*** Copyright (c) 2012-2020 by DrVrej, All rights reserved. ***
