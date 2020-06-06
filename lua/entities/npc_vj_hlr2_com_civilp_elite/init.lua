b3b3965891198eef5a431b3fa968794de4d41eb7
Mawskeeto1223 <tomaxx11@gmail.com>
2020-06-07 01:21:05 +0300

    Organizing models
    Moved Elite Metrocop files to correct folders.

--- lua/entities/npc_vj_hlr2_com_civilp_elite/init.lua
+++ lua/entities/npc_vj_hlr2_com_civilp_elite/init.lua
@@ -5,7 +5,7 @@ include('shared.lua')
 	No parts of this code or any of its contents may be reproduced, copied, modified or adapted,
 	without the prior written consent of the author, unless otherwise indicated for stand-alone materials.
 -----------------------------------------------*/
-ENT.Model = {"models/elitepolice.mdl"} -- The game will pick a random model from the table when the SNPC is spawned | Add as many as you want
+ENT.Model = {"models/vj_hlr/hl2b/elitepolice.mdl"} -- The game will pick a random model from the table when the SNPC is spawned | Add as many as you want
 ENT.StartHealth = GetConVarNumber("vj_hl2c_elitecop_h")
 
 ENT.GeneralSoundPitch1 = 80
