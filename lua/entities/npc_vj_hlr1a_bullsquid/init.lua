212f445fdff2bd997cec8988036ed53b0b6f3070
Mawskeeto1223 <tomaxx11@gmail.com>
2020-09-13 00:03:03 +0300

    wip alpha squid

---
+++ lua/entities/npc_vj_hlr1a_bullsquid/init.lua
@@ -0,0 +1,13 @@
+AddCSLuaFile("shared.lua")
+include('shared.lua')
+/*-----------------------------------------------
+	*** Copyright (c) 2012-2019 by DrVrej, All rights reserved. ***
+	No parts of this code or any of its contents may be reproduced, copied, modified or adapted,
+	without the prior written consent of the author, unless otherwise indicated for stand-alone materials.
+-----------------------------------------------*/
+ENT.Model = {""} -- The game will pick a random model from the table when the SNPC is spawned | Add as many as you want
+/*-----------------------------------------------
+	*** Copyright (c) 2012-2019 by DrVrej, All rights reserved. ***
+	No parts of this code or any of its contents may be reproduced, copied, modified or adapted,
+	without the prior written consent of the author, unless otherwise indicated for stand-alone materials.
+-----------------------------------------------*/
