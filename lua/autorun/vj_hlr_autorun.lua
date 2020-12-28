bcb24d11b8cc66c9772f91978c025ccb4f71ee57
Cpt-Hazama <Caleb123459@gmail.com>
2020-12-28 00:24:10 -0500

    Fixed name typo

--- lua/autorun/vj_hlr_autorun.lua
+++ lua/autorun/vj_hlr_autorun.lua
@@ -193,7 +193,7 @@ if VJExists == true then
 	VJ.AddNPC_HUMAN("Overwatch Engineer","npc_vj_hlr2_com_engineer",{"weapon_vj_hlr2_reager"},vCat)
 	VJ.AddNPC_HUMAN("Civil Protection","npc_vj_hlr2_com_civilp",{"weapon_vj_9mmpistol","weapon_vj_9mmpistol","weapon_vj_smg1"},vCat)
 	VJ.AddNPC_HUMAN("Civil Protection Elite","npc_vj_hlr2_com_civilp_elite",{"weapon_vj_smg1"},vCat)
-	VJ.AddNPC("Combine Ground Turret","npc_vj_hlr2_com_turret",vCat)
+	VJ.AddNPC("Overwatch Ground Turret","npc_vj_hlr2_com_turret",vCat)
 	
 	-- NPC Weapons
 	VJ.AddNPCWeapon("VJ_Combine_Sniper","weapon_vj_hlr2_csniper")
