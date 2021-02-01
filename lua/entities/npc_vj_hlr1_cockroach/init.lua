8978322202b948e5f632511a07915b4e213dcb97
DrVrej <DrVrej@users.noreply.github.com>
2021-01-31 22:20:52 -0500

    Fixed Cockroach NPC Console Error

--- lua/entities/npc_vj_hlr1_cockroach/init.lua
+++ lua/entities/npc_vj_hlr1_cockroach/init.lua
@@ -33,7 +33,7 @@ function ENT:CustomOnInitialize()
 end
 ---------------------------------------------------------------------------------------------------------------------------------------------
 function ENT:CustomOnTouch(ent)
-	if ent:IsPlayer() or entity:IsNPC() then
+	if ent:IsPlayer() or ent:IsNPC() then
 		self:TakeDamage(self:Health() + 1, ent, ent)
 		VJ_EmitSound(self, "vj_hlr/hl1_npc/roach/rch_smash.wav", 70)
 	end
