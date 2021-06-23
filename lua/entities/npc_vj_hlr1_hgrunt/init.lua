VJ-HLR-Developers/Half-Life-Resurgence

commit f3357914ad6388586c9e33428c1576e32927264d

Rebels & Alyx give ammo to player

lua/entities/npc_vj_hlr1_hgrunt/init.lua
@@ -132,35 +132,36 @@ end
 function ENT:CustomOnInitialize()
 	self:SetCollisionBounds(Vector(15, 15, 78), Vector(-15, -15, 0))
 	
-	if self:GetModel() == "models/vj_hlr/hl1/hgrunt.mdl" then // Already the default
+	local myMDL = myMDL
+	if myMDL == "models/vj_hlr/hl1/hgrunt.mdl" then // Already the default
 		self.HECU_Type = 0
 		self.HECU_WepBG = 2
-	elseif self:GetModel() == "models/vj_hlr/opfor/hgrunt.mdl" then
+	elseif myMDL == "models/vj_hlr/opfor/hgrunt.mdl" then
 		self.HECU_Type = 1
 		self.HECU_WepBG = 3
-	elseif self:GetModel() == "models/vj_hlr/opfor/hgrunt_medic.mdl" then
+	elseif myMDL == "models/vj_hlr/opfor/hgrunt_medic.mdl" then
 		self.HECU_Type = 2
 		self.HECU_WepBG = 3
 		self.AnimTbl_WeaponReload = {ACT_RELOAD_SMG1}
-	elseif self:GetModel() == "models/vj_hlr/opfor/hgrunt_engineer.mdl" then
+	elseif myMDL == "models/vj_hlr/opfor/hgrunt_engineer.mdl" then
 		self.HECU_Type = 3
 		self.HECU_WepBG = 1
 		self.AnimTbl_WeaponReload = {ACT_RELOAD_SMG1}
-	elseif self:GetModel() == "models/vj_hlr/opfor/massn.mdl" then
+	elseif myMDL == "models/vj_hlr/opfor/massn.mdl" then
 		self.HECU_Type = 4
 		self.HECU_WepBG = 2
-	elseif self:GetModel() == "models/vj_hlr/hl1/rgrunt.mdl" or self:GetModel() == "models/vj_hlr/hl1/rgrunt_black.mdl" then
+	elseif myMDL == "models/vj_hlr/hl1/rgrunt.mdl" or myMDL == "models/vj_hlr/hl1/rgrunt_black.mdl" then
 		self.HECU_Type = 5
 		self.HECU_WepBG = 1
-	elseif self:GetModel() == "models/vj_hlr/hla/hgrunt.mdl" then
+	elseif myMDL == "models/vj_hlr/hla/hgrunt.mdl" then
 		self.HECU_Type = 6
 		self.HECU_WepBG = 1
-		self.AnimTbl_Death = {ACT_DIESIMPLE,ACT_DIEFORWARD}
+		self.AnimTbl_Death = {ACT_DIESIMPLE, ACT_DIEFORWARD}
 		self.HECU_CanHurtWalk = false
-	elseif self:GetModel() == "models/vj_hlr/hl1/hassault.mdl" or self:GetModel() == "models/vj_hlr/hl_hd/hassault.mdl" then
+	elseif myMDL == "models/vj_hlr/hl1/hassault.mdl" or myMDL == "models/vj_hlr/hl_hd/hassault.mdl" then
 		self.HECU_Type = 7
 		self.HECU_WepBG = 1
-		self.AnimTbl_Death = {ACT_DIESIMPLE,ACT_DIEBACKWARD,ACT_DIEVIOLENT}
+		self.AnimTbl_Death = {ACT_DIESIMPLE, ACT_DIEBACKWARD, ACT_DIEVIOLENT}
 		self.HECU_CanHurtWalk = false
 	end
 	
@@ -367,11 +368,12 @@ function ENT:CustomOnThink()
 	end
 end
 ---------------------------------------------------------------------------------------------------------------------------------------------
+local gasTankExpPos = Vector(0, 0, 90)
 function ENT:SetUpGibesOnDeath(dmginfo, hitgroup)
 	self.HasDeathSounds = false
 	if self.HECU_GasTankHit == true then
-		util.BlastDamage(self,self,self:GetPos(),100,80)
-		util.ScreenShake(self:GetPos(),100,200,1,500)
+		util.BlastDamage(self, self, self:GetPos(), 100, 80)
+		util.ScreenShake(self:GetPos(), 100, 200, 1, 500)
 		VJ_EmitSound(self,{"vj_hlr/hl1_weapon/explosion/explode3.wav","vj_hlr/hl1_weapon/explosion/explode4.wav","vj_hlr/hl1_weapon/explosion/explode5.wav"},90)
 		
 		local spr = ents.Create("env_sprite")
@@ -387,7 +389,7 @@ function ENT:SetUpGibesOnDeath(dmginfo, hitgroup)
 		spr:SetKeyValue("framerate","15.0")
 		spr:SetKeyValue("spawnflags","0")
 		spr:SetKeyValue("scale","4")
-		spr:SetPos(self:GetPos() + Vector(0,0,90))
+		spr:SetPos(self:GetPos() + gasTankExpPos)
 		spr:Spawn()
 		spr:Fire("Kill","",0.9)
 		timer.Simple(0.9,function() if IsValid(spr) then spr:Remove() end end)
@@ -395,7 +397,7 @@ function ENT:SetUpGibesOnDeath(dmginfo, hitgroup)
 	if self.HECU_Type == 0 && hitgroup == HITGROUP_HEAD then
 		self:CreateGibEntity("obj_vj_gib","models/vj_hlr/gibs/hgib_skull.mdl",{BloodDecal="VJ_HLR_Blood_Red",Pos=self:LocalToWorld(Vector(0,0,60))})
 		self.HasDeathAnimation = false
-		return true,{DeathAnim=false,AllowCorpse=true}
+		return true, {DeathAnim=false, AllowCorpse=true}
 	else
 		if self.HasGibDeathParticles == true then
 			local bloodeffect = EffectData()
@@ -458,15 +460,16 @@ function ENT:CustomDeathAnimationCode(dmginfo, hitgroup)
 end
 ---------------------------------------------------------------------------------------------------------------------------------------------
 function ENT:CustomOnDeath_BeforeCorpseSpawned(dmginfo, hitgroup)
+	-- Remove the weapon body group
 	if self.HECU_Type == 6 or self.HECU_Type == 7 then
-		self:SetBodygroup(self.HECU_WepBG,1)
+		self:SetBodygroup(self.HECU_WepBG, 1)
 	elseif self.HECU_Type == 0 or self.HECU_Type == 3 or self.HECU_Type == 4 then
-		self:SetBodygroup(self.HECU_WepBG,2)
+		self:SetBodygroup(self.HECU_WepBG, 2)
 	elseif self.HECU_Type == 5 then
-		self:SetBodygroup(self.HECU_WepBG,2)
+		self:SetBodygroup(self.HECU_WepBG, 2)
 		self:SetSkin(4)
 	elseif self.HECU_Type == 1 or self.HECU_Type == 2 then
-		self:SetBodygroup(self.HECU_WepBG,3)
+		self:SetBodygroup(self.HECU_WepBG, 3)
 	end
 end
 ---------------------------------------------------------------------------------------------------------------------------------------------
@@ -474,8 +477,3 @@ function ENT:CustomOnDropWeapon_AfterWeaponSpawned(dmginfo, hitgroup, wepEnt)
 	wepEnt.WorldModel_Invisible = false
 	wepEnt:SetNW2Bool("VJ_WorldModel_Invisible", false)
 end
-/*-----------------------------------------------
-	*** Copyright (c) 2012-2021 by DrVrej, All rights reserved. ***
-	No parts of this code or any of its contents may be reproduced, copied, modified or adapted,
-	without the prior written consent of the author, unless otherwise indicated for stand-alone materials.
------------------------------------------------*/

lua/entities/npc_vj_hlr1_apache/init.lua
@@ -164,7 +164,7 @@ function ENT:CustomOnThink()
 	end
 end
 ---------------------------------------------------------------------------------------------------------------------------------------------
-local bulletSpread = Vector(0.05, 0.05, 0)
+local bulletSpread = Vector(0.03490, 0.03490, 0.03490)
 --
 function ENT:CustomAttack()
 	local ene = self:GetEnemy()
@@ -234,6 +234,8 @@ function ENT:CustomOnTakeDamage_AfterDamage(dmginfo, hitgroup)
 end
 ---------------------------------------------------------------------------------------------------------------------------------------------
 function ENT:CustomOnPriorToKilled(dmginfo, hitgroup)
+	self.HasDeathRagdoll = false // TODO: Add proper death corpse
+	
 	local expPos = self:GetAttachment(self:LookupAttachment("rotor")).Pos
 	local spr = ents.Create("env_sprite")
 	spr:SetKeyValue("model","vj_hl/sprites/zerogxplode.vmt")

lua/entities/npc_vj_hlr1_bullsquid/init.lua
@@ -6,7 +6,7 @@ include('shared.lua')
 	without the prior written consent of the author, unless otherwise indicated for stand-alone materials.
 -----------------------------------------------*/
 ENT.Model = {"models/vj_hlr/hl1/bullsquid.mdl"} -- The game will pick a random model from the table when the SNPC is spawned | Add as many as you want
-ENT.StartHealth = 120
+ENT.StartHealth = 100
 ENT.HullType = HULL_WIDE_SHORT
 ENT.VJC_Data = {
     FirstP_Bone = "Bip01 Spine1", -- If left empty, the base will attempt to calculate a position for first person
@@ -20,6 +20,7 @@ ENT.CustomBlood_Particle = {"vj_hl_blood_yellow"}
 ENT.CustomBlood_Decal = {"VJ_HLR_Blood_Yellow"} -- Decals to spawn when it's damaged
 ENT.HasBloodPool = false -- Does it have a blood pool?
 ENT.Immune_AcidPoisonRadiation = true -- Makes the SNPC not get damage from Acid, poison, radiation
+
 ENT.HasMeleeAttack = true -- Should the SNPC have a melee attack?
 ENT.TimeUntilMeleeAttackDamage = false -- This counted in seconds | This calculates the time until it hits something
 ENT.MeleeAttackDistance = 35 -- How close does it have to be until it attacks?
@@ -29,6 +30,7 @@ ENT.MeleeAttackKnockBack_Forward1 = 50 -- How far it will push you forward | Fir
 ENT.MeleeAttackKnockBack_Forward2 = 60 -- How far it will push you forward | Second in math.random
 ENT.MeleeAttackKnockBack_Up1 = 250 -- How far it will push you up | First in math.random
 ENT.MeleeAttackKnockBack_Up2 = 260 -- How far it will push you up | Second in math.random
+
 ENT.HasRangeAttack = true -- Should the SNPC have a range attack?
 ENT.AnimTbl_RangeAttack = {ACT_RANGE_ATTACK1} -- Range Attack Animations
 ENT.RangeAttackEntityToSpawn = "obj_vj_hlr1_toxicspit" -- The entity that is spawned when range attacking
@@ -40,6 +42,7 @@ ENT.RangeUseAttachmentForPos = false -- Should the projectile spawn on a attachm
 ENT.RangeAttackPos_Up = 20 -- Up/Down spawning position for range attack
 ENT.RangeAttackPos_Forward = 0 -- Forward/ Backward spawning position for range attack
 ENT.RangeAttackPos_Right = 0 -- Right/Left spawning position for range attack
+
 ENT.NoChaseAfterCertainRange = true -- Should the SNPC not be able to chase when it's between number x and y?
 ENT.NoChaseAfterCertainRange_FarDistance = "UseRangeDistance" -- How far until it can chase again? | "UseRangeDistance" = Use the number provided by the range attack instead
 ENT.NoChaseAfterCertainRange_CloseDistance = "UseRangeDistance" -- How near until it can chase again? | "UseRangeDistance" = Use the number provided by the range attack instead
@@ -104,21 +107,21 @@ function ENT:CustomOnAlert(ent)
 end
 ---------------------------------------------------------------------------------------------------------------------------------------------
 function ENT:RangeAttackCode_GetShootPos(projectile)
+	local ene = self:GetEnemy()
 	if self.Bullsquid_BullSquidding == true then
-		return self:CalculateProjectile("Line", self:GetPos(), self:GetEnemy():GetPos() + self:GetEnemy():OBBCenter(), 250000)
+		return self:CalculateProjectile("Line", projectile:GetPos(), ene:GetPos() + ene:OBBCenter(), 250000)
 	else
-		return self:CalculateProjectile("Curve", self:GetPos(), self:GetEnemy():GetPos() + self:GetEnemy():OBBCenter(), 1500)
+		return self:CalculateProjectile("Curve", projectile:GetPos(), ene:GetPos() + ene:OBBCenter(), 1500)
 	end
 end
 ---------------------------------------------------------------------------------------------------------------------------------------------
 function ENT:MultipleMeleeAttacks()
-	local randAttack = math.random(1,2)
-	if randAttack == 1 then
+	if math.random(1, 2) == 1 then
 		self.AnimTbl_MeleeAttack = {ACT_MELEE_ATTACK1}
 		self.MeleeAttackDamage = 35
-	elseif randAttack == 2 then
+	else
 		self.AnimTbl_MeleeAttack = {ACT_MELEE_ATTACK2}
-		self.MeleeAttackDamage = 20
+		self.MeleeAttackDamage = 25
 	end
 end
 ---------------------------------------------------------------------------------------------------------------------------------------------

lua/entities/npc_vj_hlr1_hgrunt_serg/init.lua
@@ -51,8 +51,3 @@ function ENT:CustomOnWeaponAttack()
 		self.Serg_SpinUpT = CurTime() + 4
 	end
 end
-/*-----------------------------------------------
-	*** Copyright (c) 2012-2021 by DrVrej, All rights reserved. ***
-	No parts of this code or any of its contents may be reproduced, copied, modified or adapted,
-	without the prior written consent of the author, unless otherwise indicated for stand-alone materials.
------------------------------------------------*/

lua/entities/npc_vj_hlr1_leech/init.lua
@@ -66,8 +66,3 @@ function ENT:CustomOnThink()
 		self.AnimTbl_IdleStand = {"swim"}
 	end
 end
-/*-----------------------------------------------
-	*** Copyright (c) 2012-2021 by DrVrej, All rights reserved. ***
-	No parts of this code or any of its contents may be reproduced, copied, modified or adapted,
-	without the prior written consent of the author, unless otherwise indicated for stand-alone materials.
------------------------------------------------*/

lua/entities/npc_vj_hlr1_panthereye/init.lua
@@ -63,7 +63,7 @@ ENT.GeneralSoundPitch1 = 100
 function ENT:CustomOnInitialize()
 	self:SetCollisionBounds(Vector(25, 25, 55), Vector(-25, -25, 0))
 	
-	self:SetSkin(math.random(0,1))
+	self:SetSkin(math.random(0, 1))
 end
 ---------------------------------------------------------------------------------------------------------------------------------------------
 function ENT:CustomOnAcceptInput(key, activator, caller, data)
@@ -136,8 +136,3 @@ function ENT:CustomGibOnDeathSounds(dmginfo, hitgroup)
 	VJ_EmitSound(self, "vj_gib/default_gib_splat.wav", 90, 100)
 	return false
 end
-/*-----------------------------------------------
-	*** Copyright (c) 2012-2021 by DrVrej, All rights reserved. ***
-	No parts of this code or any of its contents may be reproduced, copied, modified or adapted,
-	without the prior written consent of the author, unless otherwise indicated for stand-alone materials.
------------------------------------------------*/

lua/entities/npc_vj_hlr1_protozoan/init.lua
@@ -33,12 +33,7 @@ end
 ---------------------------------------------------------------------------------------------------------------------------------------------
 function ENT:CustomOnKilled(dmginfo, hitgroup)
 	//ParticleEffect("vj_hl_blood_boob_yellow", self:GetPos() + self:OBBCenter(), self:GetAngles())
-	local effectdata = EffectData()
-	effectdata:SetOrigin(self:GetPos() + self:OBBCenter())
-	util.Effect("VJ_HLR_Protozoan_Pop", effectdata)
+	local effectPop = EffectData()
+	effectPop:SetOrigin(self:GetPos() + self:OBBCenter())
+	util.Effect("VJ_HLR_Protozoan_Pop", effectPop)
 end
-/*-----------------------------------------------
-	*** Copyright (c) 2012-2021 by DrVrej, All rights reserved. ***
-	No parts of this code or any of its contents may be reproduced, copied, modified or adapted,
-	without the prior written consent of the author, unless otherwise indicated for stand-alone materials.
------------------------------------------------*/

lua/entities/npc_vj_hlr1_zombie/init.lua
@@ -79,10 +79,10 @@ end
 function ENT:MultipleMeleeAttacks()
 	if math.random(1, 2) == 1 then
 		self.AnimTbl_MeleeAttack = {ACT_MELEE_ATTACK1}
-		self.MeleeAttackDamage = 20
+		self.MeleeAttackDamage = 10
 	else
 		self.AnimTbl_MeleeAttack = {ACT_MELEE_ATTACK2}
-		self.MeleeAttackDamage = 40
+		self.MeleeAttackDamage = 25
 	end
 end
 ---------------------------------------------------------------------------------------------------------------------------------------------

commit d54cfdda62ced05bdaa232d08a93c7f69d6ecd7b

temp solution for hgrunt issue caused by commit f3357914ad6388586c9e33428c1576e32927264d

lua/entities/npc_vj_hlr1_hgrunt/init.lua
@@ -132,7 +132,7 @@ end
 function ENT:CustomOnInitialize()
 	self:SetCollisionBounds(Vector(15, 15, 78), Vector(-15, -15, 0))
 	
-	local myMDL = myMDL
+	local myMDL = self:GetModel()
 	if myMDL == "models/vj_hlr/hl1/hgrunt.mdl" then // Already the default
 		self.HECU_Type = 0
 		self.HECU_WepBG = 2