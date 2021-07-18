550994a8c51ab1561f8ad74173d97ac2188673a8
Cpt-Hazama <Caleb123459@gmail.com>
2021-07-18 01:05:16 -0400

    Xen Cannon Changes
    - Optimized Xen Cannon code
    - Changed Xen Cannon damage to 40
    - Reduced Xen Cannon speed to 5
    - Xen Cannon now has a 0.3 second delay when spawned before it can shoot
    - Fixed Xen Cannon always detecting it can shoot the enemy regardless of if it was aiming or not

--- lua/entities/npc_vj_hlr1_xen_cannon/init.lua
+++ lua/entities/npc_vj_hlr1_xen_cannon/init.lua
@@ -12,6 +12,7 @@ ENT.SightDistance = 6000 -- How far it can see
 ENT.SightAngle = 180 -- The sight angle | Example: 180 would make the it see all around it | Measured in degrees and then converted to radians
 ENT.MovementType = VJ_MOVETYPE_STATIONARY -- How does the SNPC move?
 ENT.CanTurnWhileStationary = false -- If set to true, the SNPC will be able to turn while it's a stationary SNPC
+ENT.PoseParameterLooking_TurningSpeed = 5
 ENT.VJC_Data = {
     ThirdP_Offset = Vector(0, 0, 0), -- The offset for the controller when the camera is in third person
     FirstP_Bone = "joint3", -- If left empty, the base will attempt to calculate a position for first person
@@ -52,9 +53,11 @@ ENT.DeathSoundLevel = 90
 
 -- Custom
 ENT.Cannon_HasLOS = false
+ENT.Cannon_LockTime = 0
 ---------------------------------------------------------------------------------------------------------------------------------------------
 function ENT:CustomOnInitialize()
 	self:SetCollisionBounds(Vector(45, 45, 65), Vector(-45, -45, 0))
+	self.Cannon_LockTime = CurTime() + 0.3 -- Prevent spawn-killing
 end
 ---------------------------------------------------------------------------------------------------------------------------------------------
 function ENT:CustomOnAcceptInput(key, activator, caller, data)
@@ -64,17 +67,22 @@ function ENT:CustomOnAcceptInput(key, activator, caller, data)
 	end
 end
 ---------------------------------------------------------------------------------------------------------------------------------------------
+local threshold = 3
+--
 function ENT:CustomOn_PoseParameterLookingCode(pitch, yaw, roll)
+	local poseYaw = self:GetPoseParameter("aim_yaw")
+	local posePitch = self:GetPoseParameter("aim_pitch")
 	-- Compare the difference between the current position of the pose parameter and the position it's suppose to go to
-	if (math.abs(math.AngleDifference(self:GetPoseParameter("aim_yaw"), math.ApproachAngle(self:GetPoseParameter("aim_yaw"), yaw, self.PoseParameterLooking_TurningSpeed))) >= 10) or (math.abs(math.AngleDifference(self:GetPoseParameter("aim_pitch"), math.ApproachAngle(self:GetPoseParameter("aim_pitch"), pitch, self.PoseParameterLooking_TurningSpeed))) >= 10) then
+	if (math.abs(math.AngleDifference(poseYaw, math.ApproachAngle(poseYaw, yaw, self.PoseParameterLooking_TurningSpeed))) >= threshold) or (math.abs(math.AngleDifference(posePitch, math.ApproachAngle(posePitch, pitch, self.PoseParameterLooking_TurningSpeed))) >= threshold) then
 		self.Cannon_HasLOS = false
 	else
 		self.Cannon_HasLOS = true
 	end
+	print(self.Cannon_HasLOS)
 end
 ---------------------------------------------------------------------------------------------------------------------------------------------
 function ENT:CustomAttackCheck_RangeAttack()
-	if self.Cannon_HasLOS == true then return true end
+	if self.Cannon_HasLOS == true && (CurTime() > self.Cannon_LockTime) then return true end
 end
 ---------------------------------------------------------------------------------------------------------------------------------------------
 function ENT:CustomRangeAttackCode()
@@ -83,11 +91,11 @@ function ENT:CustomRangeAttackCode()
 	//local endPos = (self:GetEnemy():GetPos() + self:GetEnemy():OBBCenter()) + attach.Ang:Forward()*20000 - Vector(0, 0, 10)
 	local tr = util.TraceLine({
 		start = startPos,
-		endpos = self:GetEnemy():GetPos() + self:GetEnemy():OBBCenter() + VectorRand(-50, 50),
+		endpos = self:GetEnemy():GetPos() + self:GetEnemy():OBBCenter() + VectorRand(-5, 5),
 		filter = {self} // self.extmdl
 	})
 	local hitPos = tr.HitPos
-	util.VJ_SphereDamage(self, self, hitPos, 30, 25, DMG_ENERGYBEAM, true, false, {Force=90})
+	util.VJ_SphereDamage(self, self, hitPos, 30, 40, DMG_ENERGYBEAM, true, false, {Force=90})
 	VJ_EmitSound(self, "vj_hlr/hl1_npc/xencannon/fire.wav", 90, 100)
 	sound.Play("vj_hlr/hl1_npc/pitworm/pit_worm_attack_eyeblast_impact.wav", hitPos, 60)
 	
