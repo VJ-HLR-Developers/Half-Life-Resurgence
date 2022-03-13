99b9b9ca844debb704bdd9bb8fdf6a02de348038
Cpt-Hazama <Caleb123459@gmail.com>
2022-03-13 17:32:12 -0400

    Resistance Turret Laser

--- lua/entities/npc_vj_hlr2_res_sentry/shared.lua
+++ lua/entities/npc_vj_hlr2_res_sentry/shared.lua
@@ -8,3 +8,27 @@ ENT.Instructions 	= "Click on the spawnicon to spawn it."
 ENT.Category		= "Half-Life Resurgence"
 
 ENT.HLR_Type = "Turret"
+
+if CLIENT then
+	local matLaser = Material("vj_hl/sprites/hl2/turret_laser_fade")
+	local laserColor = Color(255, 0, 0)
+    function ENT:Initialize()
+        local hookName = "VJ_HLR_TurretLaser_" .. self:EntIndex()
+        hook.Add("PreDrawEffects",hookName,function()
+            if !IsValid(self) then
+                hook.Remove("PreDrawEffects",hookName)
+                return
+            end
+
+            local attStart = self:GetAttachment(self:LookupAttachment("laser_start"))
+            local attEnd = self:GetAttachment(self:LookupAttachment("laser_end"))
+            local tr = util.TraceLine({
+                start = attStart.Pos,
+                endpos = attEnd.Pos,
+                filter = self,
+            })
+            render.SetMaterial(matLaser)
+            render.DrawBeam(tr.StartPos, tr.HitPos, 2, 0, 0.98, laserColor)
+        end)
+    end
+end
