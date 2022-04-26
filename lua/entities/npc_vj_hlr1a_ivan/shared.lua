da1bbaa9dfb2d27c6e10d71bd21015eb265069d4
DrVrej <DrVrej@users.noreply.github.com>
2022-04-26 17:06:50 -0400

    Update shared.lua

--- lua/entities/npc_vj_hlr1a_ivan/shared.lua
+++ lua/entities/npc_vj_hlr1a_ivan/shared.lua
@@ -5,13 +5,4 @@ ENT.Author 			= "DrVrej"
 ENT.Contact 		= "http://steamcommunity.com/groups/vrejgaming"
 ENT.Purpose 		= "Spawn it and fight with it!"
 ENT.Instructions 	= "Click on the spawnicon to spawn it."
-ENT.Category		= "al-Qaeda"
-
-if CLIENT then
-local Name = "yenraB"
-local LangName = "npc_vj_maw_barney"
-language.Add(LangName, Name)
-killicon.Add(LangName,"HUD/killicons/default",Color(255,80,0,255))
-language.Add("#"..LangName, Name)
-killicon.Add("#"..LangName,"HUD/killicons/default",Color(255,80,0,255))
-end
+ENT.Category		= "Half-Life Resurgence"
