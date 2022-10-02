a51fcca99961acb17fe2a26ab86235520ecac6eb
Cpt-Hazama <Caleb123459@gmail.com>
2022-10-02 03:35:20 -0400

    Added Glowing Eyes To Combine
    - Added glowing eyes to the combine to match their spawnicons (client-side)

--- lua/entities/npc_vj_hlr2_com_civilp/shared.lua
+++ lua/entities/npc_vj_hlr2_com_civilp/shared.lua
@@ -6,3 +6,24 @@ ENT.Contact 		= "http://steamcommunity.com/groups/vrejgaming"
 ENT.Purpose 		= "Spawn it and fight with it!"
 ENT.Instructions 	= "Click on the spawnicon to spawn it."
 ENT.Category		= "Half-Life Resurgence"
+
+if CLIENT then
+    local mat = Material("sprites/light_glow02_add")
+    local vecOrigin = Vector(3.6,6.75,1.7)
+    local size = 8
+    local col = Color(118,236,110)
+    function ENT:CustomOnDraw()
+        local bone = self:LookupBone("ValveBiped.Bip01_Head1")
+        local pos,ang = self:GetBonePosition(bone)
+        local glowOrigin = pos +ang:Forward() *vecOrigin.x +ang:Right() *vecOrigin.y +ang:Up() *vecOrigin.z
+
+        render.SetMaterial(mat)
+        for i = 1,2 do
+            if i == 2 then
+                glowOrigin = pos +ang:Forward() *vecOrigin.x +ang:Right() *vecOrigin.y +ang:Up() *-vecOrigin.z
+            end
+            render.DrawSprite(glowOrigin, size, size, col)
+            render.DrawSprite(glowOrigin, size, size, col)
+        end
+    end
+end
--- lua/entities/npc_vj_hlr2_com_sniper/shared.lua
+++ lua/entities/npc_vj_hlr2_com_sniper/shared.lua
@@ -6,3 +6,24 @@ ENT.Contact 		= "http://steamcommunity.com/groups/vrejgaming"
 ENT.Purpose 		= "Spawn it and fight with it!"
 ENT.Instructions 	= "Click on the spawnicon to spawn it."
 ENT.Category		= "Half-Life Resurgence"
+
+if CLIENT then
+    local mat = Material("sprites/light_glow02_add")
+    local vecOrigin = Vector(4.5,5,1.75)
+    local size = 8
+    local col = Color(169,169,169)
+    function ENT:CustomOnDraw()
+        local bone = self:LookupBone("ValveBiped.Bip01_Head1")
+        local pos,ang = self:GetBonePosition(bone)
+        local glowOrigin = pos +ang:Forward() *vecOrigin.x +ang:Right() *vecOrigin.y +ang:Up() *vecOrigin.z
+
+        render.SetMaterial(mat)
+        for i = 1,2 do
+            if i == 2 then
+                glowOrigin = pos +ang:Forward() *vecOrigin.x +ang:Right() *vecOrigin.y +ang:Up() *-vecOrigin.z
+            end
+            render.DrawSprite(glowOrigin, size, size, col)
+            render.DrawSprite(glowOrigin, size, size, col)
+        end
+    end
+end
