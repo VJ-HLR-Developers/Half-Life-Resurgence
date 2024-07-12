be256f0310c696c97b751a69a216ac497472c0e3
DrVrej <DrVrej@users.noreply.github.com>
2024-07-12 19:39:58 -0400

    Fix up the pipe melee weapon

--- lua/weapons/weapon_vj_hlr2_pipe/shared.lua
+++ lua/weapons/weapon_vj_hlr2_pipe/shared.lua
@@ -1,11 +1,12 @@
 SWEP.Base = "weapon_vj_base"
-SWEP.PrintName = "Crowbar"
+SWEP.PrintName = "Pipe"
 SWEP.Author = "DrVrej"
 SWEP.Contact = "http://steamcommunity.com/groups/vrejgaming"
 SWEP.Purpose = "This weapon is made for Players and NPCs"
 SWEP.Instructions = "Controls are like a regular weapon."
 SWEP.Category = "VJ Base"
 SWEP.MadeForNPCsOnly = true
+SWEP.ReplacementWeapon = "weapon_crowbar"
 	-- NPC Settings ---------------------------------------------------------------------------------------------------------------------------------------------
 SWEP.NPC_NextPrimaryFire = 1
 SWEP.NPC_TimeUntilFire = 0.5
@@ -15,5 +16,3 @@ SWEP.HoldType = "melee"
 	-- Primary Fire ---------------------------------------------------------------------------------------------------------------------------------------------
 SWEP.Primary.Damage = 12
 SWEP.IsMeleeWeapon = true
-SWEP.MeleeWeaponSound_Hit = {"physics/body/body_medium_break3.wav"}
-SWEP.MeleeWeaponSound_Miss = {"weapons/iceaxe/iceaxe_swing1.wav"}
