c706a77646fff3d692a2d1234c05078c153d2358
DrVrej <DrVrej@users.noreply.github.com>
2025-12-25 07:17:02 +0400

    Fixed Dreamcast alien slave textures crashing the game

--- lua/entities/npc_vj_hlr1_alienslave/init.lua
+++ lua/entities/npc_vj_hlr1_alienslave/init.lua
@@ -5,7 +5,7 @@ include("shared.lua")
 	No parts of this code or any of its contents may be reproduced, copied, modified or adapted,
 	without the prior written consent of the author, unless otherwise indicated for stand-alone materials.
 -----------------------------------------------*/
-ENT.Model = "models/vj_hlr/hl1/islave.mdl"-- Model(s) to spawn with | Picks a random one if it's a table
+ENT.Model = "models/vj_hlr/hl1/islave.mdl"
 ENT.StartHealth = 60
 ENT.SightAngle = 220
 ENT.HullType = HULL_HUMAN
