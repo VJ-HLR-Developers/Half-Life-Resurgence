5838836b41ff87a477427441ce45435ae2561f88
KM_CM <send.nex@gmail.com>
2026-01-19 09:47:54 +0200

    Eye Glow Optimization (#101)
    * Optimizations
    
    ---------
    
    Co-authored-by: DrVrej <DrVrej@users.noreply.github.com>

--- lua/entities/npc_vj_hlr2_com_elite/shared.lua
+++ lua/entities/npc_vj_hlr2_com_elite/shared.lua
@@ -5,18 +5,19 @@ ENT.Author 			= "DrVrej"
 ENT.Contact 		= "http://steamcommunity.com/groups/vrejgaming"
 ENT.Category		= "Half-Life Resurgence"
 
-if CLIENT && GetConVar("vj_hlr2_combine_eyeglow"):GetInt() == 1 then
+if CLIENT && GetConVar("vj_hlr2_combine_eyeglow"):GetBool() then
     local mat = Material("sprites/light_glow02_add")
-    local vecOrigin = Vector(5, 4.5, 0)
-    local size = 12
     local col = Color(255, 0, 0)
+	local render_SetMaterial = render.SetMaterial
+	local render_DrawSprite = render.DrawSprite
+	local size = 12
 	function ENT:Draw()
 		self:DrawModel()
         local bone = self:LookupBone("ValveBiped.Bip01_Head1")
         local pos, ang = self:GetBonePosition(bone)
-        local glowOrigin = pos + ang:Forward() * vecOrigin.x + ang:Right() * vecOrigin.y + ang:Up() * vecOrigin.z
-        render.SetMaterial(mat)
-        render.DrawSprite(glowOrigin, size, size, col)
-        render.DrawSprite(glowOrigin, size, size, col)
+        local glowOrigin = pos + ang:Forward() * 5 + ang:Right() * 4.5
+        render_SetMaterial(mat)
+        render_DrawSprite(glowOrigin, size, size, col)
+        render_DrawSprite(glowOrigin, size, size, col)
     end
 end
--- lua/entities/npc_vj_hlr2b_com_civilp_elite/shared.lua
+++ lua/entities/npc_vj_hlr2b_com_civilp_elite/shared.lua
@@ -5,23 +5,22 @@ ENT.Author 			= "DrVrej"
 ENT.Contact 		= "http://steamcommunity.com/groups/vrejgaming"
 ENT.Category		= "Half-Life Resurgence"
 
-if CLIENT && GetConVar("vj_hlr2_combine_eyeglow"):GetInt() == 1 then
+if CLIENT && GetConVar("vj_hlr2_combine_eyeglow"):GetBool() then
     local mat = Material("sprites/light_glow02_add")
-    local vecOrigin = Vector(3.75, 7, 1.75)
-    local size = 8
     local col = Color(255, 213, 123)
+	local render_SetMaterial = render.SetMaterial
+	local render_DrawSprite = render.DrawSprite
+	local size = 8
 	function ENT:Draw()
 		self:DrawModel()
         local bone = self:LookupBone("ValveBiped.Bip01_Head1")
         local pos, ang = self:GetBonePosition(bone)
-        local glowOrigin = pos + ang:Forward() * vecOrigin.x + ang:Right() * vecOrigin.y + ang:Up() * vecOrigin.z
+        local glowOrigin = pos + ang:Forward() * 3.75 + ang:Right() * 7 + ang:Up() * 1.75
         render.SetMaterial(mat)
-        for i = 1, 2 do
-            if i == 2 then
-                glowOrigin = pos + ang:Forward() * vecOrigin.x + ang:Right() * vecOrigin.y + ang:Up() * -vecOrigin.z
-            end
-            render.DrawSprite(glowOrigin, size, size, col)
-            render.DrawSprite(glowOrigin, size, size, col)
-        end
+		render_DrawSprite(glowOrigin, size, size, col)
+		render_DrawSprite(glowOrigin, size, size, col)
+		glowOrigin = pos + ang:Forward() * 3.75 + ang:Right() * 7 + ang:Up() * -1.75
+		render_DrawSprite(glowOrigin, size, size, col)
+		render_DrawSprite(glowOrigin, size, size, col)
     end
 end
