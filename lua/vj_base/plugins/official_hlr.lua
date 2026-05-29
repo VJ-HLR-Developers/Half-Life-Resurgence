be2e1e4cfda90494f17d542f31f1344d3839ec72
DrVrej <DrVrej@users.noreply.github.com>
2026-05-29 11:04:10 +0500

    Update version

--- lua/vj_base/plugins/official_hlr.lua
+++ lua/vj_base/plugins/official_hlr.lua
@@ -3,7 +3,7 @@
 	No parts of this code or any of its contents may be reproduced, copied, modified or adapted,
 	without the prior written consent of the author, unless otherwise indicated for stand-alone materials.
 -----------------------------------------------*/
-local hlrVersion = "1.3.3"
+local hlrVersion = "1.4.0"
 VJ.AddPlugin("Half-Life Resurgence", "NPC", hlrVersion)
 
 VJ.HLR_VERSION = hlrVersion
