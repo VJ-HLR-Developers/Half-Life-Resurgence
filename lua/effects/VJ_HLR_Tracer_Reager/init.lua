4f2a481b48d4c628be36b183c2e56ab9a1462035
Cpt. Hazama <Caleb123459@gmail.com>
2026-06-12 16:22:05 -0400

    Combine Reager Overhaul
    Reager was completely remade, including player usage!

---
+++ lua/effects/VJ_HLR_Tracer_Reager/init.lua
@@ -0,0 +1,132 @@
+/*-----------------------------------------------
+	*** Copyright (c) 2012-2026 by DrVrej, All rights reserved. ***
+	No parts of this code or any of its contents may be reproduced, copied, modified or adapted,
+	without the prior written consent of the author, unless otherwise indicated for stand-alone materials.
+-----------------------------------------------*/
+EFFECT.MainMat = Material("effects/bluelaser1")
+EFFECT.SecondaryMat = Material("effects/beam_nocolor")
+EFFECT.WarpMat = Material("effects/energy_swave_warp2")
+EFFECT.ImpactMat = Material("effects/energy_flare_03_nocolor")
+
+local function getStartEnt(ent)
+	if !IsValid(ent) then return end
+
+	local owner = ent:GetOwner()
+	local locPly = LocalPlayer()
+	if IsValid(owner) && owner:IsPlayer() then
+		if owner == locPly && !owner:ShouldDrawLocalPlayer() then
+			local vm = owner:GetViewModel()
+			if IsValid(vm) then
+				return vm
+			end
+		end
+	end
+	return ent
+end
+---------------------------------------------------------------------------------------------------------------------------------------------
+function EFFECT:Init(data)
+	self.StartPos = data:GetStart()
+	self.EndPos = data:GetOrigin()
+	self.DoDraw = false
+	local ent = data:GetEntity()
+	local att = data:GetAttachment()
+	local locPly = LocalPlayer()
+	self.Ent = ent
+	self.Att = att
+	self.RenderEnt = getStartEnt(self.Ent)
+
+	if IsValid(self.RenderEnt) && self.Att > 0 then
+		local att = self.RenderEnt:GetAttachment(self.Att)
+		if att then
+			self.StartPos = att.Pos
+		end
+	end
+
+	self.HitPos = self.EndPos -self.StartPos
+	self.Normal = self.HitPos:GetNormalized()
+	self:SetRenderBoundsWS(self.StartPos,self.EndPos)
+	self.NextEffectSoundT = 0
+end
+---------------------------------------------------------------------------------------------------------------------------------------------
+function EFFECT:Think()
+	-- print(self.StartPos, self.EndPos)
+	if !IsValid(self.Ent) then return false end
+	self.RenderEnt = getStartEnt(self.Ent)
+	if IsValid(self.RenderEnt) && self.Att > 0 then
+		local att = self.RenderEnt:GetAttachment(self.Att)
+		if att then
+			self.StartPos = att.Pos
+		end
+	end
+	self.DoDraw = self.Ent:GetDrawLaser() or false
+	self.EndPos = self.Ent:GetLaserHitPos() or self.EndPos
+	self:SetRenderBoundsWS(self.StartPos, self.EndPos)
+	if !self.DoDraw then return true end
+
+	local tr = util.TraceLine({
+		start = self.StartPos,
+		endpos = self.EndPos,
+		filter = self.Ent:GetOwner()
+	})
+	if tr.Hit then
+		self.Normal = tr.HitNormal
+		-- if tr.HitWorld then
+		-- 	util.Decal("FadingScorch",tr.HitPos +tr.HitNormal,tr.HitPos -tr.HitNormal)
+		-- end
+		if CurTime() > self.NextEffectSoundT then
+			sound.Play("vj_hlr/src/wep/reager/reager_hit" .. math.random(1,3) .. ".wav",tr.HitPos,50)
+			self.NextEffectSoundT = CurTime() +0.15
+		end
+		local Emitter = ParticleEmitter(self.EndPos)
+		for _ = 1, math.random(5, 15) do
+			local particle = Emitter:Add("effects/spark", self.EndPos)
+			particle:SetVelocity(VectorRand()*math.Rand(100, 350))
+			particle:SetDieTime(math.Rand(0.1, 1))
+			particle:SetStartAlpha(200)
+			particle:SetEndAlpha(0)
+			particle:SetStartSize(1)
+			particle:SetEndSize(3)
+			particle:SetRoll(math.random(0, 360))
+			particle:SetGravity(Vector(math.random(-300, 300), math.random(-300, 300), math.random(-300, -700)))
+			particle:SetCollide(true)
+			particle:SetBounce(0.9)
+			particle:SetAirResistance(120)
+			particle:SetStartLength(0)
+			particle:SetEndLength(0.1)
+			particle:SetVelocityScale(true)
+			particle:SetCollide(true)
+			particle:SetColor(185,242,255)
+		end
+		for _ = 1,8 do
+			local particle = Emitter:Add("effects/spark",self.EndPos)
+			particle:SetVelocity(math.Rand(325,425) *(self.Normal *VectorRand()):GetNormalized())
+			particle:SetDieTime(0.1)
+			particle:SetStartAlpha(255)
+			particle:SetStartSize(4)
+			particle:SetEndSize(0)
+			particle:SetStartLength(10)
+			particle:SetEndLength(0)
+			particle:SetColor(185,242,255)
+			particle:SetGravity(Vector(0,0,0))
+			particle:SetAirResistance(15)
+			particle:SetCollide(false)
+			particle:SetBounce(0)
+		end
+	end
+	return true
+end
+---------------------------------------------------------------------------------------------------------------------------------------------
+function EFFECT:Render()
+	if !self.DoDraw then return end
+	-- render.SetMaterial(self.WarpMat)
+	-- render.DrawBeam(self.StartPos,self.EndPos,math.Rand(8, 12),math.Rand(0, 1),math.Rand(0, 1) +((self.StartPos -self.EndPos):Length() /128),Color(255,255,255,255))
+
+	render.SetMaterial(self.MainMat)
+	render.DrawBeam(self.StartPos,self.EndPos,math.Rand(18, 24),math.Rand(0, 1),math.Rand(0, 1) +((self.StartPos -self.EndPos):Length() /128),Color(112,238,255,200))
+
+	render.SetMaterial(self.SecondaryMat)
+	render.DrawBeam(self.StartPos,self.EndPos,math.Rand(8, 12),math.Rand(0, 1),math.Rand(0, 1) +((self.StartPos -self.EndPos):Length() /128),Color(255,255,255,255))
+
+	render.SetMaterial(self.ImpactMat)
+	render.DrawSprite(self.EndPos,math.Rand(24,36),math.Rand(24,36),Color(188,247,255))
+end
--- lua/weapons/weapon_vj_hlr2_reager.lua
+++ lua/weapons/weapon_vj_hlr2_reager.lua
@@ -2,44 +2,119 @@ AddCSLuaFile()
 
 SWEP.Base = "weapon_vj_base"
 SWEP.PrintName = "Combine Reager"
+SWEP.Category = "Half-Life Resurgence"
 SWEP.Author = "DrVrej"
 SWEP.Contact = "http://steamcommunity.com/groups/vrejgaming"
-	-- Main Settings ---------------------------------------------------------------------------------------------------------------------------------------------
+SWEP.Spawnable = true
+SWEP.ViewModelFOV = 80
+SWEP.UseHands = true
+SWEP.ViewModel = "models/vj_hlr/hl2/weapons/c_combine_reager.mdl"
 SWEP.WorldModel = "models/vj_hlr/hl2/weapons/combine_reager.mdl"
 SWEP.HoldType = "ar2"
-SWEP.MadeForNPCsOnly = true
-	-- NPC Settings ---------------------------------------------------------------------------------------------------------------------------------------------
-SWEP.NPC_NextPrimaryFire = 0.1
+-- SWEP.MadeForNPCsOnly = true
+
+SWEP.NPC_NextPrimaryFire = 0.06
 SWEP.NPC_FiringDistanceScale = 0.15
-SWEP.NPC_ReloadSound = "weapons/physgun_off.wav"
-	-- Primary Fire ---------------------------------------------------------------------------------------------------------------------------------------------
+SWEP.NPC_ReloadSound = "vj_hlr/src/wep/reager/reager_reload.wav"
+
 SWEP.Primary.ClipSize = 200
-SWEP.Primary.Ammo = "CrossbowBolt"
-SWEP.Primary.Sound = {}
+SWEP.Primary.Delay = 0.07
+SWEP.Primary.Ammo = "AR2"
 SWEP.Primary.DisableBulletCode	= true
 SWEP.PrimaryEffects_MuzzleParticles = "vj_rifle_full_blue"
 SWEP.PrimaryEffects_SpawnShells = false
-SWEP.PrimaryEffects_DynamicLightColor = Color(0, 31, 225)
-	-- World Model ---------------------------------------------------------------------------------------------------------------------------------------------
+SWEP.PrimaryEffects_DynamicLightColor = Color(134, 217, 255)
+
+SWEP.HasReloadSound = true
+SWEP.ReloadSound = "vj_hlr/src/wep/reager/reager_reload.wav"
+
 SWEP.WorldModel_UseCustomPosition = true
 SWEP.WorldModel_CustomPositionAngle = Vector(-10, 0, 180)
 SWEP.WorldModel_CustomPositionOrigin = Vector(-1, 15, 0.5)
 ---------------------------------------------------------------------------------------------------------------------------------------------
+if CLIENT then
+    if EmissiveSys then
+        EmissiveSys:Add("models/hl_resurgence/hl2/weapons/reager",{
+            Brightness=1,
+            Mask="models/hl_resurgence/hl2/weapons/reager_i"
+        })
+	end
+
+	local glowVecOffset = Vector(0,0,0.1)
+	local glowMat = Material("sprites/light_glow02_add")
+	function SWEP:PostDrawViewModel(vm, wep, ply)
+		if !IsValid(vm) then return end
+
+		cam.Start3D(EyePos(), EyeAngles())
+			render.SetMaterial(glowMat)
+			local remainingAmmo = self:Clip1() /self.Primary.ClipSize
+			local pulse = 0.75 +math.sin(CurTime() *(4 +(1 -remainingAmmo) ^2 *20)) *0.25
+			local col = Color(255, 247, 134, 180 *pulse)
+			for i = 1, 10 do
+				local attID = vm:LookupAttachment("light" .. i)
+				if !attID or attID <= 0 then continue end
+				local att = vm:GetAttachment(attID)
+				if !att then continue end
+				render.DrawSprite(att.Pos +glowVecOffset,5 *pulse,5 *pulse,col)
+			end
+		cam.End3D()
+	end
+end
+---------------------------------------------------------------------------------------------------------------------------------------------
+function SWEP:SetupDataTables()
+	self:NetworkVar("Bool", "DrawLaser")
+	self:NetworkVar("Vector", "LaserHitPos")
+
+	self:SetDrawLaser(false)
+
+	baseclass.Get("weapon_vj_base").SetupDataTables(self)
+end
+---------------------------------------------------------------------------------------------------------------------------------------------
 function SWEP:Init()
-	-- Initialize the firing loop sounds that will be used later
 	self.NextStopFireLoop = CurTime()
-	self.FireLoop1 = CreateSound(self, "ambient/levels/citadel/zapper_loop2.wav")
-	self.FireLoop1:SetSoundLevel(80)
-	self.FireLoop2 = CreateSound(self, "ambient/levels/citadel/zapper_ambient_loop1.wav")
-	self.FireLoop2:SetSoundLevel(70)
+	self.DidStopFireLoops = true
+
+	local beam = EffectData()
+	beam:SetStart(self:GetAttachment(1).Pos)
+	beam:SetOrigin(self:GetAttachment(1).Pos)
+	beam:SetEntity(self)
+	beam:SetAttachment(1)
+	util.Effect("VJ_HLR_Tracer_Reager",beam)
 end
 ---------------------------------------------------------------------------------------------------------------------------------------------
 function SWEP:OnThink()
-	-- Stop the firing loop sound if we are no longer firing!
-	if CurTime() > self.NextStopFireLoop then
+	if CLIENT then return end
+	local curTime = CurTime()
+
+	self:SetDrawLaser(curTime < self.NextStopFireLoop)
+	if !self.DidStopFireLoops && curTime > self.NextStopFireLoop then
+		if self.FireLoop1:IsPlaying() then
+			self.FireLoop1:Stop()
+			VJ.EmitSound(self, "vj_hlr/src/wep/reager/reager_fire_end.wav", 75, 100)
+		end
 		VJ.STOPSOUND(self.FireLoop1)
-		if self.FireLoop2:IsPlaying() then self.FireLoop2:Stop() VJ.EmitSound(self, "ambient/levels/citadel/weapon_disintegrate1.wav", 75, 100) end
+		VJ.STOPSOUND(self.FireLoop2)
+		self.DidStopFireLoops = true
 	end
+
+	if !IsValid(self.Owner) then return end
+	local targetPos
+	if self.Owner:IsNPC() && IsValid(self.Owner:GetEnemy()) then
+		targetPos = self.Owner:GetEnemy():GetPos() +self.Owner:GetEnemy():OBBCenter()
+	else
+		local tr = util.TraceLine({
+			start = self.Owner:GetShootPos(),
+			endpos = self.Owner:GetShootPos() + self.Owner:GetAimVector() * 400,
+			filter = self.Owner
+		})
+		targetPos = tr.HitPos
+	end
+	if targetPos:Distance(self:GetAttachment(1).Pos) > 400 then
+		local dir = (targetPos - self:GetAttachment(1).Pos):GetNormalized()
+		targetPos = self:GetAttachment(1).Pos + dir * 400
+	end
+	local randPos = targetPos + Vector(math.Rand(-2, 2), math.Rand(-2, 2), math.Rand(-2, 2))
+	self:SetLaserHitPos(randPos)
 end
 ---------------------------------------------------------------------------------------------------------------------------------------------
 function SWEP:OwnerChanged()
@@ -50,23 +125,30 @@ end
 function SWEP:OnPrimaryAttack(status, statusData)
 	if status == "Init" then
 		if CLIENT then return end
-		local ene = self.Owner:GetEnemy()
-		if !IsValid(ene) then return end
 
-		-- Play the firing sound (hello vrej, do you really think this plays the firing sound? You have a note here so Im assuming you didn't...)
-		self.NextStopFireLoop = CurTime() + 0.2
+		self.NextStopFireLoop = CurTime() +0.1
+		self.FireLoop1 = CreateSound(self, "vj_hlr/src/wep/reager/reager_fire_loop.wav")
+		self.FireLoop1:SetSoundLevel(80)
+		self.FireLoop2 = CreateSound(self, "ambient/levels/citadel/zapper_ambient_loop1.wav")
+		self.FireLoop2:SetSoundLevel(70)
 		self.FireLoop1:Play()
 		self.FireLoop2:Play()
+		self:SetDrawLaser(true)
+		self.DidStopFireLoops = false
 
-		-- Create electrical particle and deal radius shock damage
-		local targetPos = ene:GetPos() + ene:OBBCenter()
-		if targetPos:Distance(self:GetAttachment(1).Pos) > 400 then
-			local dir = (targetPos - self:GetAttachment(1).Pos):GetNormalized()
-			targetPos = self:GetAttachment(1).Pos + dir * 400
-		end
-		local randPos = targetPos + Vector(math.Rand(-10, 10), math.Rand(-10, 10), math.Rand(-10, 10))
-		util.ParticleTracerEx("electrical_arc_01", self:GetAttachment(1).Pos, randPos, false, self:EntIndex(), 1)
-		VJ.ApplyRadiusDamage(self.Owner, self.Owner, randPos, 20, math.random(2, 5), DMG_SHOCK, true, true)
+		-- sound.Play("vj_hlr/src/wep/reager/reager_hit" .. math.random(1,3) .. ".wav",self:GetLaserHitPos(),75)
+		-- VJ.EmitSound(self,"vj_hlr/src/wep/reager/reager_fire_solo.wav",75)
+		VJ.ApplyRadiusDamage(self.Owner,self.Owner,self:GetLaserHitPos(),20,5,DMG_PLASMA,true,true)
+	end
+end
+---------------------------------------------------------------------------------------------------------------------------------------------
+function SWEP:OnReload(s)
+	if s == "Start" then
+		timer.Simple(1.4,function()
+			if IsValid(self) then
+				self:EmitSound("vj_hlr/src/wep/reager/reager_reload_end.wav", 60, 100)
+			end
+		end)
 	end
 end
 ---------------------------------------------------------------------------------------------------------------------------------------------
